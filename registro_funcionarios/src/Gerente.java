c5670e130401606a84dfd2a148611c952df6f7a9
Daniele Carvalho <44905383+daniele-mc@users.noreply.github.com>
2019-10-09 17:41:40 -0300

    Update Gerente.java

--- registro_funcionarios/src/Gerente.java
+++ registro_funcionarios/src/Gerente.java
@@ -1,7 +1,8 @@
 public class Gerente extends Funcionario {
     public Gerente(){
     }
-
+    
+    @Override
     public double calculaBonifacacao(){
         this.bonifacao = ((this.salario * 0.30) + 1000);
         return this.bonifacao;
