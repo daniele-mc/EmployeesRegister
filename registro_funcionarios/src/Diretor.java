a0fd6e28734a2b8217b6bf5ccfabf0667ad0e92f
Daniele Carvalho <44905383+daniele-mc@users.noreply.github.com>
2019-10-09 17:39:52 -0300

    Update Diretor.java

--- registro_funcionarios/src/Diretor.java
+++ registro_funcionarios/src/Diretor.java
@@ -1,7 +1,8 @@
 public class Diretor extends Funcionario {
     public Diretor(){
     }
-
+    
+    @Override
     public double calculaBonifacacao(){
         this.bonifacao = ((this.salario * 0.40) + 2000);
         return this.bonifacao;
